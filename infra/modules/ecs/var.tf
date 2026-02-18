dcc5890702770daedf3151bb8a150d16e61e9abf
Rushikesh Nikam <rushitest4559@gmail.com>
2026-02-18 11:26:37 +0530

    Update ECS variables for Fargate configuration
    Removed old EC2 variables and added new Fargate variables.

--- infra/modules/ecs/var.tf
+++ infra/modules/ecs/var.tf
@@ -5,12 +5,13 @@ variable "db_name" {}
 variable "db_username" {}
 variable "db_password" {}
 variable "public_subnet_id" {}
-variable "ecs_sg_id" {}
-variable "key_name" {
-  
-}
 variable "rds_dependency" {
   description = "RDS dependency"
   type        = any
   default     = null
 }
+
+# NEW FARGATE VARIABLES (remove old EC2 ones)
+variable "ecs_fargate_sg_id" {}           # ← Changed from ecs_sg_id
+variable "ecs_execution_role_arn" {}     # ← NEW (required for ECR pull)
+variable "ecs_task_role_arn" {}          # ← NEW (optional for app)
