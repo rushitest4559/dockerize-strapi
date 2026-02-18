064918860b153daa5ba08f25c39aefc56f421d8e
Rushikesh Nikam <rushitest4559@gmail.com>
2026-02-18 11:58:26 +0530

    Delete infra/modules/key-pair directory

--- infra/modules/key-pair/out.tf
+++
@@ -1,7 +0,0 @@
-output "key_name" {
-  value = aws_key_pair.this.key_name
-}
-
-output "private_key_path" {
-  value = local_file.private_key.filename
-}
--- infra/modules/key-pair/var.tf
+++
@@ -1,4 +0,0 @@
-variable "project_name" {
-  description = "Project name for key pair"
-  type        = string
-}
